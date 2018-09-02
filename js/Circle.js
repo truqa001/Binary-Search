0b03e9c89abce11ea2689d8a0800adfe7d0c2d27
Anthony <chumki@Anthonys-MacBook-Pro.local>
2018-09-02 21:10:30 +0930

    updated binary search

--- js/Circle.js
+++ js/Circle.js
@@ -1,5 +1,5 @@
-
-function Circle(value, CIRCLE_X, CIRCLE_Y){
+class Circle{
+    constructor(value, CIRCLE_X, CIRCLE_Y){
     this.value = value;
 
     this.CIRCLE_X = CIRCLE_X;
@@ -7,15 +7,19 @@ function Circle(value, CIRCLE_X, CIRCLE_Y){
 
     this.CIRCLE_WIDTH = 70;
     this.CIRCLE_HEIGHT = 70;
+    }
 
-
-    this.drawCircle = function(){
-        fill(255,25,25);
+    drawCircle(){
+        fill(200,150,150);
         ellipse(this.CIRCLE_X, this.CIRCLE_Y,this.CIRCLE_WIDTH, this.CIRCLE_HEIGHT)
 
         textSize(20);
         fill(255);
-        text(this.value,this.CIRCLE_X- textWidth(this.value)/2, this.CIRCLE_Y+5);
+        text(this.value,this.CIRCLE_X- textWidth(this.value)/6, this.CIRCLE_Y+5);
         textAlign(CENTER);
+    }
+
+    getValue(){
+        return this.value;
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,136 @@
 
-let array= [];
+let array;
+let complete = false;
+let search_value;
+let left_index;
+let right_index;
+let mid_index;
+let start_search;
+
 
 function setup() {
-    createCanvas(window.innerWidth,innerHeight);
-    background(0);
+    createCanvas(window.innerWidth, window.innerHeight);
+    frameRate(1);
+
+    array = new Array();
+    start_search = false;
 
 
-
-    for (var i=0;i<100;i++){
+    for (var i=0;i<=100;i++){
         let x = random(0, window.innerWidth);
         let y = random(0, window.innerHeight);
-        let a= new Circle(i, x, y);
-        a.drawCircle();
+        let circle= new Circle(i, x, y);
+        array.push(circle);
+    }
+
+    left_index = 0;
+    right_index = array.length-1;
+    mid_index = (left_index+right_index)/2 ;
+
+
+    noLoop();
+}
+
+
+function draw(){
+
+    render_Circles();
+
+    //binary search
+    if (start_search){
+
+        if (search_value == array[mid_index].getValue()){
+            for (var i=0; i<array.length;i++){
+                if (i != mid_index){
+                    array[i] = null;
+                }
+            }
+            render_Circles()
+
+            showElement("reset");
+            hideElement("loading");
+            showElement("completed");
+
+            noLoop();
+        }
+        //if value on left side, delete the right side, and vice versa
+        if (search_value < array[mid_index].getValue()){
+            for (var i=mid_index; i<=right_index;i++){
+                array[i] = null;
+            }
+            right_index = mid_index-1;
+
+        }else{
+            for (var i=0; i<= mid_index;i++){
+                array[i] = null;
+            }
+            left_index = mid_index+1;
+        }
+        mid_index = parseInt((left_index+right_index)/2) ;
     }
 
 
+
+
+
+
+}
+
+//update number of exist circles
+function render_Circles(){
+    background('#13001c');
+    for (var i=0;i<array.length;i++){
+        if (array[i] != null){
+            array[i].drawCircle();
+        }
+
+    }
+}
+
+//search button pressed
+function search() {
+    disable_input(true);
+    search_value = document.getElementById("input").value;
+
+    if (search_value > 100 || search_value < 0){
+        showElement("error");
+        hideElement("search");
+        showElement("reset");
+    }else{
+        searching()
+    }
+}
+
+//start searching
+function searching(){
+    start_search = true;
+    hideElement("search");
+    showElement("loading")
+    loop();
+}
+
+//reset screen
+function reset(){
+    set_input_value(0);
+    showElement("search");
+    hideElement("reset");
+    hideElement("completed");
+    hideElement("error");
+    disable_input(false);
+    setup();
+    draw();
+}
+
+function showElement(id){
+    document.getElementById(id).style.display = "inline-block";
+
+}
+function hideElement(id) {
+    document.getElementById(id).style.display = "none";
+}
+function disable_input(bool) {
+    document.getElementById("input").disabled = bool;
+}
+function set_input_value(val) {
+    document.getElementById("input").value = val;
 }
